461cec15aec8d991946a1b7866706177cb5a61e3
luongtvDEV <luongtv.developer@gmail.com>
2023-02-20 19:09:22 +0700

    full version :V

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 const END_POINT='http://localhost:8080/accounts';
 
 $(function(){
-    addListeners();
-    // loadAllDepartmentName();
+    addAccountListeners();
     loadAllAccounts();
+    loadAllDepartmentName();
+    addDepartmentListeners();
+    loadAllDepartments();
     
 });
-function addListeners(){
+
+
+
+// Account's Listeners
+function addAccountListeners(){
+
+    // select row 
     $('#accounts-tbody').on('click', 'tr', function(event){
         if(event.ctrlKey){
             $(this).toggleClass('selected');
@@ -34,6 +42,7 @@ function addListeners(){
         loadAllAccounts();
     });
 
+    //chang page number
     $('#page-number').on('keypress', function(event){
         //13 is key code of Enter button
         if(event.which===13){
@@ -48,12 +57,12 @@ function addListeners(){
 
     //When user click on search button
     $('#btn-search').on('click', function(event){
+        
         loadAllAccounts();
     });
 
     //When user click button create account
     $('#btn-create').on('click', function(event){
-        event.preventDefault();
         $.ajax({
             method: 'POST',
             url: 'http://localhost:8080/accounts/create-account',
@@ -68,23 +77,32 @@ function addListeners(){
             }),
             success: function(){
                 loadAllAccounts();
-                $('#create-form').reset();
+                $('#create-account-form').trigger("reset");
             }
-
         });
+        bootstrap.Modal.getOrCreateInstance($('#addModal')).hide();
+        
+    });
+
+    //when user click add button
+    $('#btn-add').on('click', function(event){
+        const departmentList = $('#create-department');
+        departmentList.empty();
+        loadAllDepartmentId();
+        
     });
 
     //when user click edit
     $('#btn-edit').on('click', function(event){
+        const departmentList = $('#update-department');
+        departmentList.empty();
+        loadAllDepartmentId();
         const row = $('.selected');
-        debugger
         $('#update-id').val(row.find('.row-id').text());
         $('#update-username').val(row.find('.row-username').text());
         $('#update-fullName').val(row.find('.row-fullName').text());
         $('#update-role').val(row.find('.row-role').text());
-        $('#update-department').val(row.find('.row-departmentName').text());
-
-        //chua show duoc department
+        $('#update-department').val(row.find('.row-departmentName').attr('value'));
     });
 
     //when user click update
@@ -101,9 +119,9 @@ function addListeners(){
             success: function(){
                 loadAllAccounts();
             }
-
-
         });
+        bootstrap.Modal.getOrCreateInstance($('#updateModal')).hide();
+
     });
 
     //when user click delete
@@ -115,12 +133,20 @@ function addListeners(){
     });
     
     //when user confirm delete
-
     $('#btn-confirm-delete').on('click', function(event){
-        
+        $.ajax({
+            method: 'DELETE',
+            url: 'http://localhost:8080/accounts/delete-accounts',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify($('.selected .row-id').toArray().map(id => id.innerText)),
+            beforeSend: () => showLoading(),
+            success: data => loadAllAccounts(),
+            complete: () => hideLoading()
+        });
+        bootstrap.Modal.getOrCreateInstance($('#deleteModal')).hide();
     });
-}
 
+}
 // filter list of accounts
 function loadAllAccounts(){
     let role=$('#role').val();
@@ -188,6 +214,134 @@ function showAllAccounts(accounts){
     }
     
 }
+//  ==========================================Department Method========================================
+//Department's Listener
+
+function addDepartmentListeners(){
+    // Chon department row
+    $('#department-tbody').on('click', 'tr', function(event){
+        if(event.ctrlKey){
+            $(this).toggleClass('department-selected');
+        }else{
+            $(this).addClass('department-selected').siblings().removeClass('department-selected');
+        }
+        const selectedRows=$('.department-selected').length;
+        if(selectedRows==0){
+            $('#btn-edit-department').attr('disabled', 'disabled');
+            $('#btn-delete-department').attr('disabled', 'disabled');
+        }
+        else if(selectedRows==1){
+            $('#btn-edit-department').removeAttr('disabled');
+            $('#btn-delete-department').removeAttr('disabled');
+        }
+        else{
+            $('#btn-edit-department').attr('disabled', 'disabled');
+            $('#btn-delete-department').attr('disabled', 'disabled');
+
+        }
+    });
+    //Reload all department when click button refresh
+    $('#btn-refresh-department').on('click', function(event){
+        loadAllDepartments();
+    });
+
+    $('#page-number-department').on('keypress', function(event){
+        //13 is key code of Enter button
+        if(event.which===13){
+            loadAllDepartments();
+        }
+    });
+
+    //When user select page size
+    $('#page-size-department').on('change', function(event){
+        loadAllDepartments();
+    });
+
+    //When user click on search
+    $('#btn-search-department').on('click', function(event){
+        loadAllDepartments();
+    });
+
+
+    //When user click create new department
+    $('#btn-create-department').on('click', function(event){
+        debugger
+        $.ajax({
+
+            method: 'POST',
+            url: 'http://localhost:8080/departments/add-department',
+            contentType: 'application/json; charset=utf-8',
+            data:JSON.stringify({
+                name: $('#create-department-name').val(),
+                type: $('#create-department-type').val(),
+                
+            }),
+            success: function(){
+                loadAllDepartments();
+                // $('#create-account-form').trigger("reset");
+            }
+        });
+        bootstrap.Modal.getOrCreateInstance($('#addDepartmentModal')).hide();
+
+        
+    });
+
+        //when user click edit
+    $('#btn-edit-department').on('click', function(event){
+        debugger
+        const row = $('.department-selected');
+        $('#update-department-id').val(row.find('.row-department-id').text());
+        $('#update-department-name').val(row.find('.row-department-name').text());
+        $('#update-department-type').val(row.find('.row-department-type').text());
+        
+    });
+    $('#btn-update-department').on('click', function(event){
+        $.ajax({
+            method: 'PUT',
+            url: 'http://localhost:8080/departments/update-department/'+$('#update-department-id').val(),
+            contentType: 'application/json; charset=utf-8',
+            data:JSON.stringify({
+                id: $('#update-department-id').val(),
+                type: $('#update-department-type').val(),
+
+            }),
+            success: function(){
+                loadAllDepartments();
+            }
+        });
+        bootstrap.Modal.getOrCreateInstance($('#updateDepartmentModal')).hide();
+
+    });
+
+     //when user click delete
+     $('#btn-delete-department').on('click', function(event){
+        const message =$('#delete-department-warning');
+        message.text(`Do you want to delete these department? `);
+        
+    });
+    
+    //when user confirm delete
+    $('#btn-confirm-delete-department').on('click', function(event){
+        const selectedRows = $('.department-selected');
+        $.ajax({
+            method: 'DELETE',
+            url: 'http://localhost:8080/departments/delete-department?id='+selectedRows.find('.row-department-id').text(),
+            success: function(){
+                         loadAllDepartments();}
+        });
+        bootstrap.Modal.getOrCreateInstance($('#deleteDepartmentModal')).hide();
+    });
+    
+}
+
+
+
+function showPageInfoDepartment(result){
+    const start = result.pageable.offset;
+    $('#page-info-department').text(`Showing ${start+1} to ${start+result.numberOfElements} of ${result.totalElements} rows.`);
+}
+
+
 function showLoading(){
 
 }
@@ -195,7 +349,7 @@ function hideLoading(){
     
 }
 
-function loadAllDepartmentName(result){
+function loadAllDepartmentName(){
     $.ajax(
         {
             method: 'GET',
@@ -208,6 +362,7 @@ function loadAllDepartmentName(result){
 }
 
 function showAllDepartmentsName(departments){
+    debugger
     const select= $('#department');
     for (const department of departments) {
         select.append(`
@@ -215,3 +370,77 @@ function showAllDepartmentsName(departments){
         `);
     }
 }
+function loadAllDepartmentId(){
+    $.ajax(
+        {
+            method: 'GET',
+            url: 'http://localhost:8080/departments',
+            success: function(result){
+                showAllDepartmentsId(result.content);
+            }
+        }
+    );
+}
+
+function showAllDepartmentsId(departments){
+    const select= $('#update-department');
+    const select2= $('#create-department');
+    for (const department of departments) {
+        select.append(`
+        <option value="${department.id}">${department.name}</option>
+        `);
+        select2.append(`
+        <option value="${department.id}">${department.name}</option>
+        `);
+    }
+}
+function loadAllDepartments(){
+    let type=$('#filter-type').val();
+    const sort= "totalMember,desc"
+    if(type==-1){
+        type=null;
+    }
+    const searchDepartmentParams= new URLSearchParams();
+    const params ={
+        page: $('#page-number-department').val(),
+        sort: sort,
+        size: $('#page-size-department').val(),
+        name: $('#search-department').val(),
+        minCreatedDate: $('#min-created-date').val(),
+        maxCreatedDate: $('#max-created-date').val(),
+        type: type
+    }
+    for (const key in params) {
+        if (params[key]) {
+            searchDepartmentParams.set(key, params[key]);
+        }
+    }
+    $.ajax(
+        {
+            method: 'GET',
+            url: 'http://localhost:8080/departments?'+searchDepartmentParams,
+            success: function(result){
+                showAllDepartments(result.content);
+                showPageInfoDepartment(result);
+            }
+        }
+    );
+}
+
+function showAllDepartments(departments){
+    const select= $('#department-tbody');
+    select.empty();
+    for (const department of departments) {
+        select.append(`
+            <tr>
+                <td class="row-department-id">${department.id}</td>
+                <td class="row-department-name">${department.name}</td>
+                <td class="row-department-totalMember">${department.totalMember}</td>
+                <td class="row-department-type">${department.type}</td>
+                <td class="row-department-createdDate">${department.createDate}</td>
+            </tr>
+        `);
+
+    }
+}
+
